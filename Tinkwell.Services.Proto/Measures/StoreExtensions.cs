3925a2daa508dfedf636e7d6dcefdbab594befd4
Adriano Repetti <adrianorepetti1977@hotmail.com>
2025-07-13 19:14:20 +0200

    Documentation

--- Tinkwell.Services.Proto/Measures/StoreExtensions.cs
+++ Tinkwell.Services.Proto/Measures/StoreExtensions.cs
@@ -4,8 +4,17 @@ using Tinkwell.Services;
 
 namespace Tinkwell.Measures;
 
+/// <summary>
+/// Helper methods for when working with the Store service.
+/// </summary>
 public static class StoreExtensions
 {
+    /// <summary>
+    /// Converts a string value to a <see cref="StoreValue"/>.
+    /// </summary>
+    /// <param name="value">The string value.</param>
+    /// <param name="timestamp">The optional timestamp. If null, <see cref="DateTime.UtcNow"/> is used.</param>
+    /// <returns>A new <see cref="StoreValue"/> instance.</returns>
     public static StoreValue ToStoreValue(this string value, DateTime? timestamp = null)
     {
         var storeValue = new StoreValue();
@@ -14,6 +23,12 @@ public static class StoreExtensions
         return storeValue;
     }
 
+    /// <summary>
+    /// Converts a double value to a <see cref="StoreValue"/>.
+    /// </summary>
+    /// <param name="value">The double value.</param>
+    /// <param name="timestamp">The optional timestamp. If null, <see cref="DateTime.UtcNow"/> is used.</param>
+    /// <returns>A new <see cref="StoreValue"/> instance.</returns>
     public static StoreValue ToStoreValue(this double value, DateTime? timestamp = null)
     {
         var storeValue = new StoreValue();
@@ -22,9 +37,20 @@ public static class StoreExtensions
         return storeValue;
     }
 
+    /// <summary>
+    /// Converts a <see cref="StoreValue"/> to its underlying <see cref="object"/> representation
+    /// (either a double or a string).
+    /// </summary>
+    /// <param name="value">The <see cref="StoreValue"/> to convert.</param>
+    /// <returns>The numeric or string value as an object.</returns>
     public static object ToObject(this StoreValue value)
         => value.HasNumberValue ? value.NumberValue : value.StringValue;
 
+    /// <summary>
+    /// Formats the <see cref="StoreValue"/> as a string, using invariant culture for numbers.
+    /// </summary>
+    /// <param name="value">The <see cref="StoreValue"/> to format.</param>
+    /// <returns>A string representation of the value.</returns>
     public static string FormatAsString(this StoreValue value)
     {
         if (value.HasNumberValue)
@@ -33,9 +59,21 @@ public static class StoreExtensions
         return value.StringValue;
     }
 
+    /// <summary>
+    /// Creates a <see cref="StoreFacade"/> from a <see cref="Store.StoreClient"/>.
+    /// Use this facade class for a simplified access to the most common Store opertions.
+    /// </summary>
+    /// <param name="client">The gRPC store client.</param>
+    /// <returns>A new <see cref="StoreFacade"/> instance.</returns>
     public static StoreFacade AsFacade(this Store.StoreClient client)
         => new StoreFacade(client);
 
+    /// <summary>
+    /// Checks if the measure definition has a specific attribute.
+    /// </summary>
+    /// <param name="definition">The measure definition.</param>
+    /// <param name="attributes">The attribute(s) to check for.</param>
+    /// <returns>True if the definition has the attribute, false otherwise.</returns>
     public static bool HasAttribute(this StoreDefinition definition, StoreMeasureAttributes attributes)
         => (definition.Attributes & (int)attributes) != 0;
 }
--- Tinkwell.Services.Proto/Measures/StoreFacade.cs
+++ Tinkwell.Services.Proto/Measures/StoreFacade.cs
@@ -3,6 +3,9 @@ using Tinkwell.Services;
 
 namespace Tinkwell.Measures;
 
+/// <summary>
+/// Provides a simplified facade over the <see cref="Store.StoreClient"/> for common measure operations.
+/// </summary>
 public sealed class StoreFacade
 {
     internal StoreFacade(Store.StoreClient client)
@@ -10,6 +13,16 @@ public sealed class StoreFacade
         _client = client;
     }
 
+    /// <summary>
+    /// Writes a numeric (double) value to a measure.
+    /// </summary>
+    /// <param name="name">
+    /// The name of the measure. This must be a numeric measure or a dynamic measure with the appropriate
+    /// units already defined.
+    /// </param>
+    /// <param name="value">The numeric value to write.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     public async Task WriteQuantityAsync(string name, double value, CancellationToken cancellationToken)
     {
         var request = new StoreUpdateRequest();
@@ -18,6 +31,17 @@ public sealed class StoreFacade
         await _client.UpdateAsync(request, cancellationToken: cancellationToken);
     }
 
+    /// <summary>
+    /// Writes a value to a measure from a string, letting the store handle the type conversion.
+    /// </summary>
+    /// <param name="name">The name of the measure.</param>
+    /// <param name="value">
+    /// The string representation of the value to write. This method performs value conversions
+    /// (when possible) if the value is not with the same unit of the registered measure. It also
+    /// performs some heuristics when there is a mismatch. It can be used also to update string measures.
+    /// </param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     public async Task WriteQuantityAsync(string name, string value, CancellationToken cancellationToken)
     {
         var request = new StoreSetMeasureValueRequest();
@@ -27,6 +51,15 @@ public sealed class StoreFacade
         await _client.SetMeasureValueAsync(request, cancellationToken: cancellationToken);
     }
 
+    /// <summary>
+    /// Writes a string value to a measure.
+    /// </summary>
+    /// <param name="name">
+    /// The name of the measure. It must be a string or dynamic measure.
+    /// </param>
+    /// <param name="value">The string value to write.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
     public async Task WriteStringAsync(string name, string value, CancellationToken cancellationToken)
     {
         var request = new StoreUpdateRequest();
